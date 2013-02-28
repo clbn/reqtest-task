015e325b64d8d9c7a5b8869380fe45b868f3abb2
Alex Olshansky <i@creagenics.com>
2013-02-28 14:57:18 +0400

    Add download links

--- docs.js
+++ docs.js
@@ -3,9 +3,10 @@ var showDocuments = function(data) {
   for (var i in documents) {
     if (documents.hasOwnProperty(i)) {
       var d = documents[i];
+      var url = './download.html?AttachmentId=' + encodeURIComponent(d.AttachmentId) + '&FileName=' + encodeURIComponent(d.FileName);
       var item = $(document.createElement('div'))
         .addClass('item')
-        .html('#' + d.AttachmentId + ' &mdash; ' + d.FileName + '<br/>(' + d.FileSize + ', ' + d.DateAdded + ')');
+        .html('#' + d.AttachmentId + ' &mdash; <a href="' + url + '">' + d.FileName + '</a><br/>(' + d.FileSize + ', ' + d.DateAdded + ')');
       $('#doc-list').append(item);
     }
   }
